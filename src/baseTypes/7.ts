cb02516c799906a6cb051122afed6ce2f8864945
IgorMushk <igor.mushketik@gmail.com>
2023-10-26 18:27:25 +0300

    upd - Basic types
    9.ts
    const date1 = new Date('December 17, 1995 03:24:00');
    console.log(typeof(date1));   // object

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,17 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayOfWeek {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+const currentDay = DayOfWeek.Saturday;
+//const currentDay = DayOfWeek.Monday;
+
+function isWeekend(day: DayOfWeek): boolean {
+ if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday ) {
+  return true
+ }
+ else  return false;
+}
+
+const currentDayIs = isWeekend(currentDay) ? 'Current day is day off' : 'Current day is working';
+console.log(currentDayIs);
